98591cfd158cacb80bc16f735349dcc202e7e78d
Jaroslav Lobačevski <jarlob@gmail.com>
2020-08-06 13:19:46 +0300

    Optimization

--- src/Utilities/FlowAnalysis/FlowAnalysis/Analysis/TaintedDataAnalysis/SanitizerInfo.cs
+++ src/Utilities/FlowAnalysis/FlowAnalysis/Analysis/TaintedDataAnalysis/SanitizerInfo.cs
@@ -69,6 +69,8 @@ namespace Analyzer.Utilities.FlowAnalysis.Analysis.TaintedDataAnalysis
         /// </summary>
         public bool RequiresValueContentAnalysis => false;
 
+        public string? FullDependencyTypeName => null;
+
         public override int GetHashCode()
         {
             return HashUtilities.Combine(this.SanitizingMethods,
